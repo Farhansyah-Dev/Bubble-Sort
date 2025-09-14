644278ff06b386bb371fbc69069922eb504fc39e
Farhansyah-Dev <farhansyah.it@gmail.com>
2025-09-14 10:05:54 +0700

    Menambahkan Komentar pada setiap baris kode

--- main.py
+++ main.py
@@ -1,17 +1,21 @@
-def bubbleSort (arr):
-    panjangData = len(arr)
+def bubbleSort(arr): #Fungsi Bubble Sort
+    panjang_Data = len(arr) #Hitung Panjang List
     
-    for i in range (panjangData):
+    for i in range (panjang_Data): #Looping untuk setiap putaran
+        swapped = False
         
-        for j in range (0, panjangData - i - 1):
+        for j in range (0, panjang_Data - i - 1): #Looping untuk membandingkan element didalam list
             
-            if arr[j] > arr[j + 1]:
+            if arr[j] > arr[j + 1]: #jika elemen yang ditemukan lebih besar dibandingkan elemen setelahnya maka tukar posisi
                 arr[j], arr[j + 1] = arr[j + 1], arr[j]
+            swapped = True
+        if swapped == False:
+            break
                 
-    return arr
+    return arr #Mengembalikan nilai arr
 
-if __name__ == "__main__":
-    data = [12, 81, 41, 1, 21, 60]
-    print(f'Data sebelum diurutkan: {data}')
+if __name__ == "__main__": #Run jika program dijalankan langsung
+    data = [12, 81, 41, 1, 21, 60] #Variabel yang menampung list data
+    print(f'Data sebelum diurutkan: {data}') #Menampilkan data sebelum di urutkan
     
-    print(f'Data setelah dilakukan Bubble Sort {bubbleSort(data)}')
+    print(f'Data setelah dilakukan Bubble Sort {bubbleSort(data)}') #Menampilkan data setelah diurutkan

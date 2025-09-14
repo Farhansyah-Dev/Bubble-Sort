37e84d9133ecbc2174386127ce6ee2da0436ee7a
Farhansyah-Dev <farhansyah.it@gmail.com>
2025-09-14 10:47:20 +0700

    Program Bubble Sort

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
+import re
 def bubbleSort(arr): #Fungsi Bubble Sort
+    
+    if not all (isinstance(x, int) for x in arr): #cek apakah nilai dalam list integer semua
+        raise ValueError ('Semua Data harus berupa angka.')
+    
     panjang_Data = len(arr) #Hitung Panjang List
     
     for i in range (panjang_Data): #Looping untuk setiap putaran
@@ -15,7 +20,27 @@ def bubbleSort(arr): #Fungsi Bubble Sort
     return arr #Mengembalikan nilai arr
 
 if __name__ == "__main__": #Run jika program dijalankan langsung
-    data = [12, 81, 41, 1, 21, 60] #Variabel yang menampung list data
-    print(f'Data sebelum diurutkan: {data}') #Menampilkan data sebelum di urutkan
+    user_input = input('Masukan beberapa angka yang ingin diurutkan: ') #fitur untuk user
     
-    print(f'Data setelah dilakukan Bubble Sort {bubbleSort(data)}') #Menampilkan data setelah diurutkan
+    user_list = re.split(r'[,\s]+', user_input.strip()) #memecahkan input user menjadi list berdasarkan koma dan spaci
+    
+    data = [] #menampung data valid
+    invalid_elements = [] #menampung data tidak valid
+    # data = [12, 81, 41, 1, "dua satu", 60] #Variabel yang menampung list data
+    
+    for x in user_list: #memeriksa elemen pada user list
+        try:
+            data.append(int(x)) #coba ubah integer, jika berhasi tambahkan ke data
+        except ValueError:
+            invalid_elements.append(x) #jika gagal, maka masuk dalam invalid argumen
+            
+    if invalid_elements: #jika ada element tidak valid tampilkan pada user
+        print(f"❌ Elemen berikut bukan angka dan dilewatkan: {invalid_elements}")
+        
+    if data: #jika data valid tampilkan data sebelum di urutkan dan sesudah
+        print(f"Data valid sebelum Bubble Sort: {data}")
+        print(f"Data setelah Bubble Sort: {bubbleSort(data)}")
+        
+    else: #jika semua data tidak valid
+        print("⚠️ Tidak ada data valid untuk diurutkan.")
+    
